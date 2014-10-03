d3c344cd1883b70d745b01926341cfc15d8a25a6
Liz Cole <liz.cole1227@gmail.com>
2014-10-03 10:08:43 -0500

    "added if.php"

---
+++ if.php
@@ -0,0 +1,40 @@
+<?php
+
+$a = 5;
+$b = 10;
+$c = '10';
+
+
+if ($a < $b) {
+	echo "$a less than $b \n";
+}
+
+if ($a > $b) {
+	echo "$a greater than $b \n";
+}
+
+if ($b >= $c) {
+	echo "$b is greater than or equal to $c \n";
+}
+
+if ($b <= $c) {
+	echo "$b is less than or equal to $c \n";
+}
+
+if ($b == $c) {
+	echo "$b is equal to $c \n";
+}
+
+if ($b === $c) {
+	echo "$b is equivalant to $c \n";
+}
+
+if ($b != $c) {
+	echo "$b is not equal to $c \n";
+}
+
+if ($b !== $c) {
+	echo "$b is not identical to $c \n";
+}
+
+?>
